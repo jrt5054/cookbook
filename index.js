168dc846889ef3c1a8b5d8b788e645d34bb2ba1c
jrt5054 <JoshuaRTaylor87@gmail.com>
2021-11-23 15:34:00 -0500

    install and utilize cors

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 require('dotenv').config();
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
